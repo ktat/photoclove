41f48910830426f50c334b00b277e46f3ca9862b
ktat <ktat.is@gmail.com>
2023-01-29 23:44:51 +0900

    improve getting Exif data.

--- src-tauri/src/value/meta.rs
+++ src-tauri/src/value/meta.rs
@@ -1,7 +1,7 @@
 use crate::value::file;
-use rexif;
-use serde::{Deserialize,Serialize};
 use regex;
+use rexif;
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MetaData {
@@ -12,12 +12,24 @@ pub struct MetaData {
     pub Make: String,
     pub LensMake: String,
     pub Model: String,
+    pub XResolution: String,
+    pub YResolution: String,
+    pub ResolutionUnit: String,
+    pub Copyright: String,
+    pub ExposureTime: String,
+    pub ShutterSpeedValue: String,
+    pub FocalLength: String,
+    pub FocalLengthIn35mmFilm: String,
+    pub DigitalZoomRatio: String,
+    pub ExposureMode: String,
+    pub WhiteBalanceMode: String,
+    pub Orientation: String,
     // TODO
 }
 
 impl MetaData {
     pub fn empty() -> MetaData {
-        MetaData{
+        MetaData {
             ISO: String::from(""),
             FNumber: String::from(""),
             DateTime: String::from(""),
@@ -25,39 +37,65 @@ impl MetaData {
             Make: String::from(""),
             Model: String::from(""),
             LensMake: String::from(""),
-         }
+            XResolution: String::from(""),
+            YResolution: String::from(""),
+            ResolutionUnit: String::from(""),
+            Copyright: String::from(""),
+            ExposureTime: String::from(""),
+            ShutterSpeedValue: String::from(""),
+            FocalLength: String::from(""),
+            FocalLengthIn35mmFilm: String::from(""),
+            DigitalZoomRatio: String::from(""),
+            ExposureMode: String::from(""),
+            WhiteBalanceMode: String::from(""),
+            Orientation: String::from(""),
+        }
     }
 
     pub fn new(file: file::File) -> MetaData {
         let exif_data = rexif::parse_file(file.path.to_string());
-        let mut data  = MetaData::empty();
-
+        let mut data = MetaData::empty();
         if exif_data.is_ok() {
             for e in exif_data.unwrap().entries {
                 match e.tag {
-                    rexif::ExifTag::FNumber => { data.FNumber = e.value_more_readable.to_string() }
-                    rexif::ExifTag::ISOSpeedRatings => { data.ISO = e.value_more_readable.to_string()}
-                    rexif::ExifTag::DateTime => { data.DateTime = e.value_more_readable.to_string() }
-                    rexif::ExifTag::LensModel => { data.LensModel = e.value_more_readable.to_string() }
-                    rexif::ExifTag::LensMake => {data.LensMake = e.value_more_readable.to_string()},
-                    rexif::ExifTag::Make => {data.Make = e.value_more_readable.to_string()},
-                    rexif::ExifTag::Model => {data.Model = e.value_more_readable.to_string()},
+                    _ => {
+                        // eprintln!("{:?}", e.tag);
+                        // eprintln!("{:?}", e.ifd.ext_data);
+                        // eprintln!("{:?}", e.value.to_string());
+                        // eprintln!("{:?}", e.value_more_readable.to_string());
+                    }
+                }
+            }
+            let exif_data = rexif::parse_file(file.path.to_string());
+            for e in exif_data.unwrap().entries {
+                match e.tag {
+                    rexif::ExifTag::FNumber => data.FNumber = e.value_more_readable.to_string(),
+                    rexif::ExifTag::ISOSpeedRatings => data.ISO = e.value_more_readable.to_string(),
+                    rexif::ExifTag::DateTime => data.DateTime = e.value_more_readable.to_string(),
+                    rexif::ExifTag::LensModel => data.LensModel = e.value.to_string(),
+                    rexif::ExifTag::LensMake => data.LensMake = e.value.to_string(),
+                    rexif::ExifTag::Make => data.Make = e.value_more_readable.to_string(),
+                    rexif::ExifTag::Model => data.Model = e.value_more_readable.to_string(),
                     // rexif::ExifTag::UnknownToMe => todo!(),
                     // rexif::ExifTag::ImageDescription => todo!(),
-                    // rexif::ExifTag::Orientation => todo!(),
-                    // rexif::ExifTag::XResolution => todo!(),
-                    // rexif::ExifTag::YResolution => todo!(),
-                    // rexif::ExifTag::ResolutionUnit => todo!(),
+                    rexif::ExifTag::Orientation => {
+                        data.Orientation = e.value_more_readable.to_string()
+                    }
+                    rexif::ExifTag::XResolution => data.XResolution = e.value.to_string(),
+                    rexif::ExifTag::YResolution => data.YResolution = e.value.to_string(),
+                    rexif::ExifTag::ResolutionUnit => data.ResolutionUnit = e.value.to_string(),
                     // rexif::ExifTag::Software => todo!(),
                     // rexif::ExifTag::HostComputer => todo!(),
                     // rexif::ExifTag::WhitePoint => todo!(),
                     // rexif::ExifTag::PrimaryChromaticities => todo!(),
                     // rexif::ExifTag::YCbCrCoefficients => todo!(),
                     // rexif::ExifTag::ReferenceBlackWhite => todo!(),
-                    // rexif::ExifTag::Copyright => todo!(),
+                    rexif::ExifTag::Copyright => data.Copyright = e.value.to_string(),
                     // rexif::ExifTag::ExifOffset => todo!(),
                     // rexif::ExifTag::GPSOffset => todo!(),
-                    // rexif::ExifTag::ExposureTime => todo!(),
+                    rexif::ExifTag::ExposureTime => {
+                        data.ExposureTime = e.value_more_readable.to_string()
+                    }
                     // rexif::ExifTag::ExposureProgram => todo!(),
                     // rexif::ExifTag::SpectralSensitivity => todo!(),
                     // rexif::ExifTag::OECF => todo!(),
@@ -65,7 +103,9 @@ impl MetaData {
                     // rexif::ExifTag::ExifVersion => todo!(),
                     // rexif::ExifTag::DateTimeOriginal => todo!(),
                     // rexif::ExifTag::DateTimeDigitized => todo!(),
-                    // rexif::ExifTag::ShutterSpeedValue => todo!(),
+                    rexif::ExifTag::ShutterSpeedValue => {
+                        data.ShutterSpeedValue = e.value.to_string()
+                    }
                     // rexif::ExifTag::ApertureValue => todo!(),
                     // rexif::ExifTag::BrightnessValue => todo!(),
                     // rexif::ExifTag::ExposureBiasValue => todo!(),
@@ -74,9 +114,19 @@ impl MetaData {
                     // rexif::ExifTag::MeteringMode => todo!(),
                     // rexif::ExifTag::LightSource => todo!(),
                     // rexif::ExifTag::Flash => todo!(),
-                    // rexif::ExifTag::FocalLength => todo!(),
+                    rexif::ExifTag::FocalLength => {
+                        data.FocalLength = e.value_more_readable.to_string()
+                    }
+                    rexif::ExifTag::FocalLengthIn35mmFilm => {
+                        data.FocalLengthIn35mmFilm = e.value_more_readable.to_string()
+                    }
                     // rexif::ExifTag::SubjectArea => todo!(),
-                    // rexif::ExifTag::MakerNote => todo!(),
+                    rexif::ExifTag::MakerNote => {
+                        let d = get_lens_from_maker_note(e.ifd.ext_data);
+                        if d != "" {
+                            data.LensModel = d;
+                        }
+                    }
                     // rexif::ExifTag::UserComment => todo!(),
                     // rexif::ExifTag::FlashPixVersion => todo!(),
                     // rexif::ExifTag::ColorSpace => todo!(),
@@ -92,10 +142,15 @@ impl MetaData {
                     // rexif::ExifTag::SceneType => todo!(),
                     // rexif::ExifTag::CFAPattern => todo!(),
                     // rexif::ExifTag::CustomRendered => todo!(),
-                    // rexif::ExifTag::ExposureMode => todo!(),
-                    // rexif::ExifTag::WhiteBalanceMode => todo!(),
-                    // rexif::ExifTag::DigitalZoomRatio => todo!(),
-                    // rexif::ExifTag::FocalLengthIn35mmFilm => todo!(),
+                    rexif::ExifTag::ExposureMode => {
+                        data.ExposureMode = e.value_more_readable.to_string()
+                    }
+                    rexif::ExifTag::WhiteBalanceMode => {
+                        data.WhiteBalanceMode = e.value_more_readable.to_string()
+                    }
+                    rexif::ExifTag::DigitalZoomRatio => {
+                        data.DigitalZoomRatio = e.value_more_readable.to_string()
+                    }
                     // rexif::ExifTag::SceneCaptureType => todo!(),
                     // rexif::ExifTag::GainControl => todo!(),
                     // rexif::ExifTag::Contrast => todo!(),
@@ -146,4 +201,49 @@ impl MetaData {
         }
         data
     }
+}
+
+// currently only for Panasonic camera
+fn get_lens_from_maker_note(data: Vec<u8>) -> String {
+    let mut str = String::new();
+    let mut i = 0;
+
+    let re = regex::Regex::new("(LUMIX|LEICA|OLYMPUS)$").unwrap();
+    while i < data.len() {
+        let d = data[i];
+        let c = std::char::from_u32(d.into());
+        match c {
+            Some(c) => str.push(c),
+            _ => (),
+        }
+        if str.len() == 9 && str != "Panasonic" {
+            // eprintln!("{:?}", str);
+            return "".to_string();
+        }
+
+        if re.is_match(&str) {
+            let cap = re.captures(&str).unwrap();
+            if cap.len() > 0 {
+                let name = &cap[0];
+                let mut i2 = i;
+                let mut lens = name.to_string();
+                while i2 < data.len() {
+                    i2 += 1;
+                    if data[i2] < 32 || 126 < data[i2] {
+                        eprintln!("{:?}", lens);
+                        return lens.to_string();
+                    }
+                    let c = std::char::from_u32(data[i2].into());
+                    match c {
+                        Some(c) => lens.push(c),
+                        _ => {
+                            return lens.to_string();
+                        }
+                    }
+                }
+            }
+        }
+        i += 1;
+    }
+    return "".to_string();
 }
--- src/PhotoInfo.jsx
+++ src/PhotoInfo.jsx
@@ -27,11 +27,16 @@ function PhotoInfo(props) {
                             <tr><th>File Name</th><td>{props.path.replace(/^.+\//, '')}</td></tr>
                             <tr><th>ISO</th><td>{photoInfo.ISO}</td></tr>
                             <tr><th>FNumber</th><td>{photoInfo.FNumber}</td></tr>
+                            <tr><th>Shutter Speed</th><td>{photoInfo.ExposureTime}</td></tr>
                             <tr><th>LensModel</th><td>{photoInfo.LensModel}</td></tr>
                             <tr><th>LensMake</th><td>{photoInfo.LensMake}</td></tr>
                             <tr><th>Make</th><td>{photoInfo.Make}</td></tr>
                             <tr><th>Model</th><td>{photoInfo.Model}</td></tr>
                             <tr><th>Date & Time</th><td>{photoInfo.DateTime}</td></tr>
+                            <tr><th>Focal Length</th><td>{photoInfo.FocalLength == photoInfo.FocalLengthIn35mmFilm ? photoInfo.FocalLength : photoInfo.FocalLength + "(" + photoInfo.FocalLengthIn35mmFilm + ")"}</td></tr>
+                            <tr><th>Digital Zoom Ratio</th><td>{photoInfo.DigitalZoomRatio}</td></tr>
+                            <tr><th>Exposure Mode</th><td>{photoInfo.ExposureMode}</td></tr>
+                            <tr><th>WhiteBalance Mode</th><td>{photoInfo.WhiteBalanceMode}</td></tr>
                         </tbody>
                     </table>
                 )}
