e768081858226e836521a66b69ed6a1726f3658b
ktat <ktat.is@gmail.com>
2023-02-21 09:44:10 +0900

    lock and warning message for directory maintenance operation

--- src/DirectoryMenu.jsx
+++ src/DirectoryMenu.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { invoke, convertFileSrc } from "@tauri-apps/api/tauri";
+import { message, confirm } from "@tauri-apps/api/dialog";
 
 
 function DirectoryMenu(props) {
@@ -11,6 +12,37 @@ function DirectoryMenu(props) {
         setPhotoIndex(l - 1)
     }, [props.photoSelection])
 
+    let lock = false;
+    async function create_db_in_date() {
+        if (lock) {
+            message("Currently, this operation is locked. Pelase wait for a while", "This operation is locked");
+        } else {
+            confirm("This takes long time if you have many photos.", "Warning").then((answer) => {
+                if (answer) {
+                    lock = true;
+                    invoke("create_db_in_date", { dateStr: props.currentDate }).then(() => {
+                        lock = false;
+                    })
+                }
+            });
+        }
+    }
+
+    async function move_photos_to_exif_date() {
+        if (lock) {
+            message("Currently, this operation is locked. Pelase wait for a while", "This operation is locked");
+        } else {
+            confirm("This takes long time if you have many photos.", "Warning").then((answer) => {
+                if (answer) {
+                    lock = true;
+                    invoke("move_photos_to_exif_date", { dateStr: props.currentDate }).then(() => {
+                        lock = false;
+                    })
+                }
+            });
+        }
+    }
+
     return (
         <div id="directory-maintenance" className="rightMenu">
             <ul className="tabs-list">
@@ -20,8 +52,8 @@ function DirectoryMenu(props) {
             </ul>
             <div id="tab-maintenance" className={props.tabClass['maintenance'] ? "tab-active" : "tab"}>
                 <ul>
-                    <li><a href="#" onClick={() => { invoke("create_db_in_date", { dateStr: props.currentDate }) }}>(re)Create database of the date</a></li>
-                    <li><a href="#" onClick={() => { invoke("move_photos_to_exif_date", { dateStr: props.currentDate }) }}>Move files according to Exif date</a></li>
+                    <li><a href="#" onClick={() => { create_db_in_date() }}>(re)Create database of the date</a></li>
+                    <li><a href="#" onClick={() => { move_photos_to_exif_date() }}>Move files according to Exif date</a></li>
                 </ul>
             </div>
             <div id="tab-filter" className={props.tabClass['filter'] ? "tab-active" : "tab"}>
