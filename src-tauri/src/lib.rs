62405b3c8e0f02a779a86fc4c8901a616f49e0a2
Atsushi Kato <ktat.is@gmail.com>
2026-03-01 11:43:44 +0900

    fix

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -72,7 +72,7 @@ fn queue_startup_insights(config: &entity::config::Config, app_handle: tauri::Ap
     domain_service::job_queue::executor::process_new_jobs(db_arc, 1, app_handle);
 }
 
-#[cfg_attr(
+#![cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
